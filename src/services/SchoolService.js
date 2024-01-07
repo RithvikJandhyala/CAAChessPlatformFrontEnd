285135361fdc1de9b8e621ac780d60a2842433a1
RithvikJandhyala <67884428+RithvikJandhyala@users.noreply.github.com>
2024-01-06 21:15:47 -0700

    Update SchoolService.js

--- src/services/SchoolService.js
+++ src/services/SchoolService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const SERVER_URL = 'http://localhost:8080';
+const SERVER_URL = 'https://azcaachessbackend-2c61c505bb61.herokuapp.com';
 const SCHOOLS_REST_API_URL = SERVER_URL + '/findAllSchools';
 const SCHOOL_DELETE = SERVER_URL + '/deleteSchool';
 const SCHOOLS_ADD = SERVER_URL + '/schools/add';
